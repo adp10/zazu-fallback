6061713501e17f43a94708ea4c905ae89067dba2
rajat <rajatbhardwaj@recko.io>
2020-08-22 00:51:07 +0530

    Added some shortcuts

--- searches.js
+++ searches.js
@@ -1,5 +1,5 @@
 const searches = {
-  amazon: { name: 'Amazon', url: 'https://www.amazon.com/s?url=search-alias=aps&field-keywords='},
+  amazon: { name: 'Amazon', url: 'https://www.amazon.in/s?url=search-alias=aps&field-keywords='},
   duck: { name: 'DuckDuckGo', url: 'https://duckduckgo.com/?q='},
   giphy: { name: 'Giphy', url: 'https://giphy.com/search/'},
   gh: { name: 'GitHub', url: 'https://github.com/search?utf8=%E2%9C%93&q='},
@@ -12,10 +12,12 @@ const searches = {
   translate: { name: 'Google Translate', url: 'https://translate.google.com/?text='},
   twitter: { name: 'Twitter', url: 'https://twitter.com/search?q='},
   stack: { name: 'Stack Overflow', url: 'https://stackoverflow.com/search?q='},
+  so: { name: 'Stack Overflow', url: 'https://stackoverflow.com/search?q='},
   ud: { name: 'Urban Dictionary', url: 'https://www.urbandictionary.com/define.php?term='},
   wiki: { name: 'Wikipedia', url: 'https://wikipedia.org/wiki/Special:Search/'},
   wolf: { name: 'Wolfram Alpha', url: 'https://www.wolframalpha.com/input/?i='},
   youtube: { name: 'YouTube', url: 'https://www.youtube.com/results?search_query='},
+  yt: { name: 'YouTube', url: 'https://www.youtube.com/results?search_query='},
   thing: { name: 'Thingiverse', url: 'http://www.thingiverse.com/search?q='},
 }
 
